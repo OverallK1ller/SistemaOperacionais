c844774a4dfe2faddc82f9b022afb6dcaa72ce5f
Matheus Voltolini <mathe.voltolini+github@gmail.com>
2025-04-14 14:14:04 -0300

    agora tem que testar cerinho mas deve funcionar

--- Cliente.cpp
+++ Cliente.cpp
@@ -1,9 +1,87 @@
 #include <iostream>
-#include <locale.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <cstring>
+using namespace std;
+#include <iostream>
+#include <fcntl.h>
+#include <unistd.h>
+#include <cstring>
 #include "Servidor.h"
 
 using namespace std;
-int main(){
-cout<< "palhaço";
+
+void menu() {
+    cout << "Escolha a operação:" << endl;
+    cout << "1. Inserir produto (INSERT)" << endl;
+    cout << "2. Deletar produto (DELETE)" << endl;
+    cout << "3. Atualizar produto (UPDATE)" << endl;
+    cout << "4. Consultar produto (SELECT)" << endl;
+    cout << "5. Sair" << endl;
 }
-  
+
+int main() {
+    int fd;
+    string comando;
+    int opcao;
+
+    // Cria o pipe (FIFO) para comunicação com o servidor
+    fd = open("requisicoes_fifo", O_WRONLY);
+    if (fd == -1) {
+        perror("Erro ao abrir o pipe");
+        return 1;
+    }
+
+    while (true) {
+        menu();
+        cout << "Escolha uma opção: ";
+        cin >> opcao;
+
+        switch (opcao) {
+            case 1: {
+                string nome;
+                cout << "Digite o nome do produto: ";
+                cin >> nome;
+                comando = "INSERT," + nome;
+                break;
+            }
+            case 2: {
+                int id;
+                cout << "Digite o id do produto a ser removido: ";
+                cin >> id;
+                comando = "DELETE," + to_string(id);
+                break;
+            }
+            case 3: {
+                int id;
+                string nome;
+                cout << "Digite o id do produto a ser atualizado: ";
+                cin >> id;
+                cout << "Digite o novo nome do produto: ";
+                cin >> nome;
+                comando = "UPDATE," + to_string(id) + "," + nome;
+                break;
+            }
+            case 4: {
+                int id;
+                cout << "Digite o id do produto a ser consultado: ";
+                cin >> id;
+                comando = "SELECT," + to_string(id);
+                break;
+            }
+            case 5:
+                cout << "Saindo..." << endl;
+                close(fd);
+                return 0;
+            default:
+                cout << "Opção inválida. Tente novamente." << endl;
+                continue;
+        }
+
+        // Envia o comando para o servidor via pipe
+        write(fd, comando.c_str(), comando.size() + 1);
+        cout << "Comando enviado: " << comando << endl;
+    }
+
+    return 0;
+}
--- Servidor.cpp
+++ Servidor.cpp
@@ -1,1 +1,99 @@
 
+#include <iostream>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <pthread.h>
+#include <cstring>
+#include "Servidor.h"
+
+using namespace std;
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER; // Mutex global
+Lde lista;
+
+void* processa_requisicao(void* arg) {
+    string comando = *(string*)arg;
+    delete (string*)arg;
+
+    pthread_mutex_lock(&mutex);
+
+    if (comando.find("INSERT") == 0) {
+        string nome = comando.substr(7); // extrai nome após "INSERT,"
+        No novo;
+        novo.id = (lista.fim ? lista.fim->id + 1 : 1);
+        strncpy(novo.nome, nome.c_str(), 50);
+        inserirFinalLde(lista, novo);
+        cout << "[INSERT] Nome inserido: " << nome << endl;
+
+    } else if (comando.find("DELETE") == 0) {
+        int id = stoi(comando.substr(7));
+        if (retirarLde(lista, id)) {
+            cout << "[DELETE] Registro ID " << id << " removido." << endl;
+        } else {
+            cout << "[DELETE] ID " << id << " não encontrado." << endl;
+        }
+
+    } else if (comando.find("SELECT") == 0) {
+        int id = stoi(comando.substr(7));
+        No* aux = lista.comeco;
+        while (aux != nullptr) {
+            if (aux->id == id) {
+                cout << "[SELECT] ID: " << aux->id << ", Nome: " << aux->nome << endl;
+                break;
+            }
+            aux = aux->eloP;
+        }
+        if (aux == nullptr)
+            cout << "[SELECT] ID " << id << " não encontrado." << endl;
+
+    } else if (comando.find("UPDATE") == 0) {
+        size_t pos1 = comando.find(",");
+        size_t pos2 = comando.find(",", pos1 + 1);
+        if (pos1 != string::npos && pos2 != string::npos) {
+            int id = stoi(comando.substr(pos1 + 1, pos2 - pos1 - 1));
+            string novo_nome = comando.substr(pos2 + 1);
+            No* aux = lista.comeco;
+            while (aux != nullptr) {
+                if (aux->id == id) {
+                    strncpy(aux->nome, novo_nome.c_str(), 50);
+                    cout << "[UPDATE] ID " << id << " alterado para: " << novo_nome << endl;
+                    break;
+                }
+                aux = aux->eloP;
+            }
+            if (aux == nullptr)
+                cout << "[UPDATE] ID " << id << " não encontrado." << endl;
+        } else {
+            cout << "[UPDATE] Comando mal formatado." << endl;
+        }
+
+    } else {
+        cout << "[ERRO] Comando não reconhecido: " << comando << endl;
+    }
+
+    pthread_mutex_unlock(&mutex);
+    return nullptr;
+}
+
+int main() {
+    mkfifo("requisicoes_fifo", 0666);
+    int fd = open("requisicoes_fifo", O_RDONLY);
+
+    inicializarLde(lista);
+
+    while (true) {
+        char buffer[128] = {0};
+        int n = read(fd, buffer, sizeof(buffer));
+        if (n > 0) {
+            string* comando = new string(buffer);
+            pthread_t thread;
+            pthread_create(&thread, nullptr, processa_requisicao, comando);
+            pthread_detach(thread); // liberação automática após fim
+        }
+    }
+
+    close(fd);
+    unlink("requisicoes_fifo");
+    return 0;
+}
